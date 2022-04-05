e6604db9e0a6f14d47bde5d1ec216d5251c01fcd
miguelstolze <miguelstolze.ms@gmail.com>
2022-04-05 19:57:20 -0300

    criate project

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-import HelloWolrd from './componets/HelloWolrd'
-
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Container from "./componets/LayOut/Container";
+import Company from "./componets/Pages/Company";
+import Contact from "./componets/Pages/Contact";
+import Home from "./componets/Pages/Home";
+import NewProject from "./componets/Pages/NewProject";
 function App() {
-
-  const nome = "Miguel"
-  const newName = nome.toUpperCase()
-  const url = 'https://via.placeholder.com/150'
-
-  function sun(a, b){
-    return a + b
-  }
-
   return (
-    <div className="App">
-      <h1>Olá {newName}</h1>
-      <p>soma: {sun(1, 2)}</p>
-      <img src={url} alt="my-image"/>
-      <HelloWolrd/>
-      
-      
-    </div>
+    <Router>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/Company">Company</Link>
+        <Link to="/Contact">Contact</Link>
+        <Link to="/NewProject">NewProject</Link>
+      </ul>
+      <Container customClass="min-height">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/Company" element={<Company />}></Route>
+          <Route path="/Contact" element={<Contact />}></Route>
+          <Route path="/NewProject" element={<NewProject />}></Route>
+        </Routes>
+      </Container>
+
+      <p>Footer</p>
+    </Router>
   );
 }
 
